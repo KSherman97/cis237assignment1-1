538530768c97e4612742b9761cf7cc223ec24e85
Kyle Sherman <ksherm337@gmail.com>
2017-01-25 00:45:59 -0500

    added more documentation for the wineItem class

--- assignment1/WineItem.cs
+++ assignment1/WineItem.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/**
+ * Kyle Sherman
+ * CIS 237 - Advanced C# Programming
+ * 1/25/2017
+**/
+
+// standard imports
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +15,16 @@ namespace assignment1
 {
     class WineItem
     {
-
+        // ******************************** 
+        //          Backing Fields         
+        // ********************************
         private string _wineItemID;
         private string _wineItemDescription;
         private string _wineItemVolume;
 
+        // ******************************** 
+        //            Properties                             
+        // ********************************
         public string WineItemIDString
         {
             get { return _wineItemID; }
@@ -31,6 +43,11 @@ namespace assignment1
             set { _wineItemVolume = value; }
         }
 
+        // ******************************** 
+        //          Constructors                  
+        // ********************************
+        
+        // 3 parameter constructor, passes in the wineItemID, description, and volume
         public WineItem(string wineItemIDString, string wineItemDescriptionString, string wineItemVolumeString)
         {
             this._wineItemID = wineItemIDString;
@@ -38,10 +55,16 @@ namespace assignment1
             this._wineItemVolume = wineItemVolumeString;
         }
 
-        public WineItem() { }
+        public WineItem() { } // Blank Constructor
 
+        // ToString override method
+        // this method makes the WineItem.ToString() 
+        // display a concatinated string of the 3 vars
+        // wineID, wineName, and wineVolume
         public override string ToString()
         {
+            // the this keyword is used to reference 'this' class. 
+            // it allows us to reference things that are declard at this class @ 'class level'
             return this._wineItemID + " " + this._wineItemVolume + " " + this._wineItemVolume;
         }
     }
